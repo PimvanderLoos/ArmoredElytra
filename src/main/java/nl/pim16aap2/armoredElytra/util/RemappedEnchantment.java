c6d11241627212f493ba21b5ce5f2537f09d8d7b
Pim van der Loos <pimvanderloos@gmail.com>
2024-05-28 18:37:35 +0200

    Improve enchantment parsing from config
    - Made the enchantment parsing more lenient. `unbreaking`, `durability`, `minecraft:unbreaking`, and `minecraft:durability` will now properly get matched to either `Enchantment#UNBREAKING` or `Enchantment#DURABILITY` depending on the server version.
    - Moved the enchantment parsing code out of the config to reduce clutter.
    - Refactored the RemappedEnchantment class so it can be used to access the remapped names.

--- src/main/java/nl/pim16aap2/armoredElytra/util/RemappedEnchantment.java
+++ src/main/java/nl/pim16aap2/armoredElytra/util/RemappedEnchantment.java
@@ -5,32 +5,108 @@ import org.bukkit.Registry;
 import org.bukkit.enchantments.Enchantment;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * A class containing mappings for enchantments that have different names in different versions of Minecraft.
  */
 public final class RemappedEnchantment
 {
-    public static final Enchantment UNBREAKING =
-        findEnchantment("UNBREAKING", "DURABILITY");
+    private static final RemappedEnchantment REMAPPED_UNBREAKING =
+        new RemappedEnchantment("unbreaking", "durability");
 
-    public static final Enchantment PROTECTION_ENVIRONMENTAL =
-        findEnchantment("PROTECTION", "PROTECTION_ENVIRONMENTAL");
+    private static final RemappedEnchantment REMAPPED_PROTECTION_ENVIRONMENTAL =
+        new RemappedEnchantment("protection", "protection_environmental");
 
-    public static final Enchantment PROTECTION_FIRE =
-        findEnchantment("FIRE_PROTECTION", "PROTECTION_FIRE");
+    private static final RemappedEnchantment REMAPPED_PROTECTION_FIRE =
+        new RemappedEnchantment("fire_protection", "protection_fire");
 
-    public static final Enchantment PROTECTION_FALL =
-        findEnchantment("FEATHER_FALLING", "PROTECTION_FALL");
+    private static final RemappedEnchantment REMAPPED_PROTECTION_FALL =
+        new RemappedEnchantment("feather_falling", "protection_fall");
 
-    public static final Enchantment PROTECTION_EXPLOSIONS =
-        findEnchantment("BLAST_PROTECTION", "PROTECTION_EXPLOSIONS");
+    private static final RemappedEnchantment REMAPPED_PROTECTION_EXPLOSIONS =
+        new RemappedEnchantment("blast_protection", "protection_explosions");
 
-    public static final Enchantment PROTECTION_PROJECTILE =
-        findEnchantment("PROJECTILE_PROTECTION", "PROTECTION_PROJECTILE");
+    private static final RemappedEnchantment REMAPPED_PROTECTION_PROJECTILE =
+        new RemappedEnchantment("projectile_protection", "protection_projectile");
+
+    private static final List<RemappedEnchantment> REMAPPED_ENCHANTMENTS = List.of(
+        REMAPPED_UNBREAKING,
+        REMAPPED_PROTECTION_ENVIRONMENTAL,
+        REMAPPED_PROTECTION_FIRE,
+        REMAPPED_PROTECTION_FALL,
+        REMAPPED_PROTECTION_EXPLOSIONS,
+        REMAPPED_PROTECTION_PROJECTILE
+    );
+
+    /*
+     * The resulting enchantments.
+     */
+    public static final Enchantment UNBREAKING = REMAPPED_UNBREAKING.getEnchantment();
+
+    public static final Enchantment PROTECTION_ENVIRONMENTAL = REMAPPED_PROTECTION_ENVIRONMENTAL.getEnchantment();
+
+    public static final Enchantment PROTECTION_FIRE = REMAPPED_PROTECTION_FIRE.getEnchantment();
+
+    public static final Enchantment PROTECTION_FALL = REMAPPED_PROTECTION_FALL.getEnchantment();
+
+    public static final Enchantment PROTECTION_EXPLOSIONS = REMAPPED_PROTECTION_EXPLOSIONS.getEnchantment();
+
+    public static final Enchantment PROTECTION_PROJECTILE = REMAPPED_PROTECTION_PROJECTILE.getEnchantment();
 
 
-    private static Enchantment findEnchantment(String... names)
+    /**
+     * The names of the enchantment in different versions of Minecraft.
+     */
+    private final List<String> names;
+
+    private final Enchantment enchantment;
+
+    private RemappedEnchantment(String... names)
+    {
+        this.names = List.of(names);
+        this.enchantment = findEnchantment();
+    }
+
+    /**
+     * Gets all remapped enchantments.
+     *
+     * @return The remapped enchantments.
+     */
+    public static List<RemappedEnchantment> getRemappedEnchantments()
+    {
+        return REMAPPED_ENCHANTMENTS;
+    }
+
+    /**
+     * Gets the names of the enchantment in different versions of Minecraft.
+     *
+     * @return The names of the enchantment in different versions of Minecraft.
+     */
+    public List<String> getNames()
+    {
+        return names;
+    }
+
+    /**
+     * Gets the enchantment that corresponds to one of the {@link #names}.
+     *
+     * @return The enchantment one of the {@link #names} corresponds to.
+     */
+    public Enchantment getEnchantment()
+    {
+        return enchantment;
+    }
+
+    /**
+     * Finds the enchantment that corresponds to one of the {@link #names}.
+     *
+     * @return The enchantment one of the {@link #names} corresponds to.
+     *
+     * @throws IllegalArgumentException
+     *     If none of the {@link #names} correspond to an enchantment.
+     */
+    private Enchantment findEnchantment()
     {
         for (final String name : names)
         {
